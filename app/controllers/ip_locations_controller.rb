7425abe0249b96f7543fe6fffd46cd4be8691013
Taehwa Kang <taehwa.kang@dramancompany.com>
2023-10-15 21:53:54 +0900

    feat: Add country API

--- app/controllers/ip_locations_controller.rb
+++ app/controllers/ip_locations_controller.rb
@@ -5,4 +5,9 @@ class IpLocationsController < ApplicationController
   def index
     render json: IpApi::Client.new.location(params[:ip]).to_json
   end
+
+  # GET /ip_country
+  def country
+    render json: { country: IpApi::Client.new.location(params[:ip]).country }
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,5 @@ Rails.application.routes.draw do
   get 'up' => 'rails/health#show', as: :rails_health_check
 
   resources :ip_locations, only: :index
+  get 'ip_country' => 'ip_locations#country'
 end
